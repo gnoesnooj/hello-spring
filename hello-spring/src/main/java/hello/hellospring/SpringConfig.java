d134d92782bf5b95ef6ba6fea5e86e3c204da958
gnoesnooj <91658692+gnoesnooj@users.noreply.github.com>
2021-11-26 21:30:30 +0900

    refactor : SpringDataJPA 로 수정

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -9,24 +9,15 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
 
-    private final DataSource dataSource;
-    private final EntityManager em;
+    // @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    } // Spring data jpa가 인터페이스를 보고 구현체를 만들어 놓은게 여기로 주입이 되어서 등록된다..
 
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
-    }
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
-
 }
